2b3f8a8cbf4a3a9f71b11387bbd3742c1d3726d2
Yash Bhaskar Narkhede <98454362+Yashnarkhede16@users.noreply.github.com>
2022-05-11 08:37:10 +0530

    Add files via upload

---
+++ JOption.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package food.ordering.system1;
+
+/**
+ *
+ * @author abc
+ */
+class JOption {
+
+    static void showMessageDialog(Register aThis, String data_Inserted, String success, int INFORMATION_MESSAGE) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
